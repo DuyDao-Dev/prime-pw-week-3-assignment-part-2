62dc2b033d5fd675685810ac2537bf1cf9e3a9a4
Duy Dao <jimmydao@gmail.com>
2021-05-19 08:05:55 -0500

    still a work in progress. Stuck in an infinite loop on line 34

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -6,35 +6,47 @@ console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
 console.log('count from 0 to 3');
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i=0; i<4; i++) {  
+for (let i=0; i<4; i++) {
   console.log(i);
 }
 
-// 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
+// 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+for (let i=0; i<6; i++) {
+  console.log(i);
+}//The part of the example loop that we would need to change would be the number 4
+//to a 6 in the parameter. We want the number to be 1 more than the comparison value so that
+//when we run our loop, it would eventually result in false to stop the loop.
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+for (var i = 3; i<6; i++) {
+  console.log(i);
+}//We would need to define i to 3 and change the length value to generate a false output at 6
+//to stop counting at 5.
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+for (var i = 2; i<12; i+2) {
+  console.log(i);
+}//Currently a work in progress. I'm stuck in an infinite loop.
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a counddown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
 
-// 2. For of loops  
+// 2. For of loops
 console.log('---- 2. For Of loop ----');
-let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
+let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb'];
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
 
 
-// 3. While loops  
+// 3. While loops
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
